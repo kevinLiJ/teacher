417215cb3596ffaa8417709bb05bcb3189e06e4d
lijianyang <ljy812118@163.com>
2017-04-15 22:05:33 +0800

    teacherregister

--- controllers/ajax.js
+++ controllers/ajax.js
@@ -2,5 +2,14 @@ var query = require('../models/query.js');
 // 封装ajax处理函数
 // teacher注册
 exports.teacherRegister = function(req, res) {
-    res.send('success');
+    var data = [];
+    for (var key in req.body) {
+        data.push(req.body[key])
+    }
+    query('INSERT INTO teacher(email,password,username) VALUES(?,?,?)', data, function(error, result) {
+        if (error) {
+            res.json({ success: false })
+        }
+        res.json({ success: true })
+    })
 }
--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 var userController = require('../controllers/user');
+var ajaxController = require('../controllers/ajax');
 
 router
 // 当请求路径是login时，处理函数是showLogin
@@ -12,6 +13,6 @@ router
 
 
 //ajax路由
-
-// 导出路由
+.post('/teacher/register', ajaxController.teacherRegister)
+    // 导出路由
 module.exports = router;
